c277b1d2682c3a8e9c86305b6291103997a51bce
Avashkhadka <avash2063@gmail.com>
2024-07-15 16:59:36 +0545

    neew update

--- js/app.js
+++ js/app.js
@@ -10,3 +10,27 @@ cross.addEventListener("click",()=>{
 
   
 })
+
+
+
+let dropdown=document.querySelector(".dropdown");
+let dropdowndata=document.querySelector(".js-aboutus");
+let dropdowndatali=document.querySelectorAll(".js-aboutus li a");
+dropdown.addEventListener("click",(e)=>{
+  e.preventDefault();
+  dropdowndata.classList.toggle("js-aboutus-show");
+  dropdowndata.classList.toggle(".js-aboutus-show li a");
+
+  // alert("hi")
+})
+
+
+let dropdownRes=document.querySelector("#aboutus-res");
+let dropdowndataRes=document.querySelector(".js-aboutus-res");
+dropdownRes.addEventListener("click",(e)=>{
+  e.preventDefault();
+  dropdowndataRes.classList.toggle("js-aboutus-res-show");
+
+  // alert("hi")
+})
+
